44558f595df02217cef9a0eae1d8eae9be69d09f
Mohammed Khursiwala <mkhursiwala2005@users.noreply.github.com>
2026-04-19 22:36:58 +0000

    Implemented enemy grid initialization, movement, edge detection, and rendering in game.c

--- src/game.c
+++ src/game.c
@@ -18,6 +18,24 @@ void game_loop() {
     	player.width = 100;
     	player.height = 80;
 
+	// enemy array
+	Enemy enemies[ENEMY_ROWS][ENEMY_COLS];
+
+	// initializing enemies
+	for (int row = 0; row < ENEMY_ROWS; row++) {
+    		for (int col = 0; col < ENEMY_COLS; col++) {
+        		enemies[row][col].x = ENEMY_START_X + col * (ENEMY_WIDTH + ENEMY_PADDING_X);
+        		enemies[row][col].y = ENEMY_START_Y + row * (ENEMY_HEIGHT + ENEMY_PADDING_Y);
+
+        		enemies[row][col].width = ENEMY_WIDTH;
+        		enemies[row][col].height = ENEMY_HEIGHT;
+
+        		enemies[row][col].alive = 1;
+    		}
+	}
+
+	int enemyDirection = 1; // 1 = right, -1 = left
+
     	while (1) {
 
         	// INPUT (movement)
@@ -38,11 +56,61 @@ void game_loop() {
             		player.x = getFramebufferWidth() - player.width;
         	}
 
-		// DELAY
-                delay();
+		// Add edge dectection
+		int moveDown = 0;
+
+		for (int row = 0; row < ENEMY_ROWS; row++) {
+    			for (int col = 0; col < ENEMY_COLS; col++) {
+
+        			if (!enemies[row][col].alive) continue;
+
+        			if (enemies[row][col].x <= 0 ||
+    				   enemies[row][col].x + enemies[row][col].width >= SCREEN_WIDTH) {
+
+            				moveDown = 1;
+        			}
+    			}
+		}
+
+		// Move enemies
+		for (int row = 0; row < ENEMY_ROWS; row++) {
+    			for (int col = 0; col < ENEMY_COLS; col++) {
+
+        			if (!enemies[row][col].alive) continue;
+
+        			if (moveDown) {
+            				enemies[row][col].y += 20;
+        			} else {
+            				enemies[row][col].x += enemyDirection * 1; // smoother
+        			}
+    			}
+		}
+
+		if (moveDown) {
+    			enemyDirection *= -1;
+		}
 
 	        // RENDER
         	clearScreen(0xff0000); // red background
         	drawRect(player.x, player.y, player.width, player.height, 0xffffff);
+
+		// Draw enemies
+		for (int row = 0; row < ENEMY_ROWS; row++) {
+    			for (int col = 0; col < ENEMY_COLS; col++) {
+
+        			if (enemies[row][col].alive) {
+            				drawRect(
+                				enemies[row][col].x,
+                				enemies[row][col].y,
+                				enemies[row][col].width,
+                				enemies[row][col].height,
+                				0x00ff00
+            				);
+        			}
+    			}
+		}
+
+		// DELAY
+                delay();
     	}
 }
